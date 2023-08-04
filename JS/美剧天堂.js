3d73687d9a23b6ce33412eebae05478846b1f6a9
圆周率 LR <lr.ya@qq.com>
2023-08-04 19:04:05 +0000

    Replace 美剧天堂.js

--- JS/美剧天堂.js
+++ JS/美剧天堂.js
@@ -2,24 +2,23 @@ var rule = {
     title:'美剧天堂',
     host:'https://wap.meijutt.tv',
     //url:'/fypage_fyclass------.html',
-    url:'/file/listfyclass_(fypage+1).html',
+    url:'/file/listfyclass_fypage.html',
     searchable:2,//是否启用全局搜索,
     quickSearch:1,//是否启用快速搜索,
     headers:{
         'User-Agent':'MOBILE_UA'
     },
     timeout:5000,//网站的全局请求超时,默认是3000毫秒
-    class_name:'魔幻科幻&灵异惊悚&都市情感&犯罪历史&选秀综艺&动漫卡通',
-    class_url:'1&2&3&4&5&6',
+    class_parse: '.index-nav li:gt(0):lt(5);a&&Text;a&&href;.*/(.*?).html',
     play_parse:true,
 	lazy:'',
     limit:6,
     推荐:'.tab-content&&li;*;*;*;*',
     一级:'.book-li;.book-title&&Text;.listimg&&img&&src;.book-cell&&em&&Text;a&&href',
     二级:{
-        "title":".picture&&title;.data--span:eq(0)&&Text",
-        "img":".picture&&img&&data-original",
-        "desc":".pic-text:eq(0)&&Text;;;.data--span:eq(1)&&Text;.data--span:eq(2)&&Text",
+        "title":".detailHeader&&h1",
+        "img":".pic&&img&&src",
+        "desc":".jianjie&&ul&&li:eq(0)&&Text;;;.data--span:eq(1)&&Text;.data--span:eq(2)&&Text",
         "content":".desc--a&&Text",
         "tabs":".nav-tabs&&li",
         "lists":".tab-pane:eq(#id)&&li"
