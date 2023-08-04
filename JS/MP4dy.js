27d96e8a59937421e9f6e0c456242453f615625c
圆周率 LR <lr.ya@qq.com>
2023-08-04 06:27:41 +0000

    Replace MP4dy.js

--- JS/MP4dy.js
+++ JS/MP4dy.js
@@ -2,7 +2,7 @@ var rule = {
 	title:'MP4dy',
 	host:'https://m.mp4us.com',
 	// url: '/fyclass/index_(fypage-1).html',
-	url: '/list/fyfilter.html',
+	url: '/list/fyclass.html',
 	searchUrl: '/search/;post?wd={wd}&nobot=1',
 	filter_url:'-{{fl.class or "0"}}',
 	filter:{
