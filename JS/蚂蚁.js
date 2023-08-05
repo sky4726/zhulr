15b86b721ecbe19ef28b3afb30b1f4076e999776
圆周率 LR <lr.ya@qq.com>
2023-08-05 15:08:41 +0000

    Replace 蚂蚁.js

--- JS/蚂蚁.js
+++ JS/蚂蚁.js
@@ -2,15 +2,27 @@ var rule = {
 	title:'蚂蚁',
 	host:'https://www.mayi4k.com',
 	url:'/vodshow/fyclass--------fypage---.html',
-	filterable:0,//是否启用分类筛选,
-	headers:{
-		'User-Agent': 'MOBILE_UA'
-	},
-	timeout:5000,
+	searchable:2,//是否启用全局搜索,
+    quickSearch:1,//是否启用快速搜索,
+    headers:{
+        'User-Agent': 'MOBILE_UA',
+    },
+    timeout:5000,
 	class_name:'蓝光圆盘&4K电影&剧集&演唱会',
 	class_url:'266&269&247&246',
-	一级:'.module-item;.a&&title;.lazyloaded&&data-src;.module-item-caption&&span:eq(0);.a&&href',
-	推荐:'.tab-list.active;a.module-poster-item.module-item;.module-poster-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href',
-	searchUrl:'/index.php/ajax/suggest?mid=fypage&wd=**',
-	搜索:'json:list;name;pic;;id',
+    play_parse:true,
+	lazy:'',
+    limit:6,
+    推荐:'*',
+    一级:'.module-item;.a&&title;.lazyloaded&&data-src;.module-item-caption&&span:eq(0);.a&&href',
+    二级:{
+        'title':'.pic&&img&&title',
+        'img':'.pic&&img&&src',
+        'desc':'.jianjie&&ul&&li:eq(4)&&Text',
+        'content':'.jianjie&&ul&&span&&Text',
+        'tabs':'#playtab&&.cili',
+        'lists':'#playtab&&a[href^="magnet:"]'
+    },
+    searchUrl:'/index.php/ajax/suggest?mid=fypage&wd=**',
+    搜索:'json:list;name;pic;;id',
 }
