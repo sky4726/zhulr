e8982b3b35c2b7a41cbabf06f1bb2ca1f71dfe90
圆周率 LR <lr.ya@qq.com>
2023-08-05 09:08:45 +0000

    Replace 美剧天堂.js

--- JS/美剧天堂.js
+++ JS/美剧天堂.js
@@ -2,7 +2,7 @@ var rule = {
     title:'美剧天堂',
     host:'https://wap.meijutt.tv',
     //url:'/fypage_fyclass------.html',
-    url:'/file/listfyclass.html',
+    url:'/file/listfyclass_(fypage+1).html',
     searchable:2,//是否启用全局搜索,
     quickSearch:1,//是否启用快速搜索,
     headers:{
@@ -10,9 +10,8 @@ var rule = {
         'Content-Type':'text/html; charset=gbk'
     },
     timeout:5000,//网站的全局请求超时,默认是3000毫秒
-    class_parse:'.col1;a&&Text;a&&href;.*/(.*?).html',
-    //class_name:'魔幻科幻&灵异惊悚&都市情感&犯罪历史&选秀综艺&动漫卡通',
-    //class_url:'1&2&3&4&5&6',
+    class_name:'魔幻科幻&灵异惊悚&都市情感&犯罪历史&选秀综艺&动漫卡通',
+    class_url:'1&2&3&4&5&6',
     play_parse:true,
 	lazy:'',
     limit:6,
