00b84ab6e13b76d8f071ac0e84d1947f767ec544
圆周率 LR <lr.ya@qq.com>
2023-08-05 11:19:42 +0000

    Replace 美剧天堂.js

--- JS/美剧天堂.js
+++ JS/美剧天堂.js
@@ -23,7 +23,7 @@ var rule = {
         'desc':'.jianjie&&ul&&li:eq(4)&&Text',
         'content':'.jianjie&&ul&&span&&Text',
         'tabs':'#playtab&&.cili',
-        'lists':'#playtab&&li a[href^="magnet:"]:eq(#id)'
+        'lists':'#playtab&&.fn-clear:eq(#id) li'
     },
     searchUrl:'/index.php/ajax/suggest?mid=fypage&wd=**',
     搜索:'json:list;name;pic;;id',
