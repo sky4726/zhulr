f488ffb74cfd2350636ac1e8037647e2931936e4
圆周率 LR <lr.ya@qq.com>
2023-08-05 22:32:57 +0000

    Replace MP4dy.js

--- JS/MP4dy.js
+++ JS/MP4dy.js
@@ -1,87 +1,29 @@
 var rule = {
-	title:'MP4dy',
-	host:'https://m.mp4us.com',
-	// url: '/fyclass/index_(fypage-1).html',
-	url: '/list/fyclass-fypage.html',
-	searchUrl: '/search/;post?wd={wd}&nobot=1',
-	searchable:2,
-	quickSearch:1,
-	headers:{
-		'User-Agent': 'MOBILE_UA'
-	},
-	timeout:5000,
-	class_name:'剧集&动漫&记录片',
-	class_url:'10&9&8',
-	play_parse:true,
+    title:'MP4电影网',
+    host:'https://m.mp4us.com',
+    url:'/list/fyclass-fypage.html',
+    //url:'/file/listfyclass_(fypage+1).html',
+    searchable:2,//是否启用全局搜索,
+    quickSearch:1,//是否启用快速搜索,
+    headers:{
+        'User-Agent': 'MOBILE_UA',
+    },
+    timeout:5000,//网站的全局请求超时,默认是3000毫秒
+    class_name:'动漫&剧集&记录',
+    class_url:'9&10&8',
+    play_parse:true,
 	lazy:'',
-	limit:6,
-	推荐: `js:
-		pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
-		var d = [];
-		var html = request(input);
-		var list = pdfa(html, '#list_all&&ul&&li');
-		list.forEach(it => {
-			d.push({
-				title: pdfh(it, 'alt=\'《&&》'),
-				desc: pdfh(it, 'rate badge\'>&&</span>'),
-				pic_url: pd(it, 'data-original=\'&&\''),
-				url: pd(it, 'href=\'&&\'')
-			});
-		})
-		setResult(d);
-	`,
-	一级: `js:
-		pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
-		var d = [];
-		if (MY_CATE !== '1' && MY_CATE !== '2') {
-			let turl = (MY_PAGE === 1)? 'index' : 'index_'+ MY_PAGE;
-			input = HOST + MY_CATE + turl + '.html';
-		}
-		var html = request(input);
-		var list = pdfa(html, '#list_all&&ul&&li');
-		list.forEach(it => {
-			d.push({
-				title: pdfh(it, 'img&&alt=\'《&&》'),
-				desc: pdfh(it, 'a&&rate badge\'>&&</span>'),
-				pic_url: pd(it, 'img&&data-original=\'&&\''),
-				url: pd(it, 'a&&href=\'&&\'')
-			});
-		})
-		setResult(d);
-	`,
-	二级:{
-		title:'h1&&Text;p.mb-2:eq(4)&&Text',
-		desc:'p.mb-2:eq(1)&&Text;;;p.mb-2:eq(7)&&Text;p.mb-2:eq(5)&&Text',
-		img:'.rounded-2&&src',
-		content:'.mv_card_box&&Text',
-		tabs:'js:TABS = ["磁力播放"]',
-		lists:`js:
-		log(TABS);
-		pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
-		LISTS = [];
-		var dd=[];
-		TABS.forEach(function(tab) {
-			if (/磁力播放/.test(tab)) {
-				var d = pdfa(html, '.mv_down&&a[href^="magnet:"]');
-				d = d.map(function(it) {
-					var title = pdfh(it, 'Text');
-					log('title >>>>>>>>>>>>>>>>>>>>>>>>>>' + title);
-					var burl = pd(it, 'href');
-					log('burl >>>>>>>>>>>>>>>>>>>>>>>>>>' + burl);
-					return title + '$' + burl
-				});
-				LISTS.push(d)
-			} else if (/在线预览/.test(tab)) {
-				var d = pd(html, 'iframe&&src');
-				if (d) {
-					d=['第一集在线播放预览$' + d]
-				} else {
-					d=['没有预览不要点$']
-				}
-				LISTS.push(d)
-			}
-		});
-		`,
-	},
-	搜索:'.col;h2&&Text;.card-img&&style;.me-auto&&Text;a&&href',
+    limit:6,
+    推荐:'*',
+    一级:'#list_all&&li;.img&&alt=\'《&&》;.img&&data-original;badge\">&&</span>;a&&href',
+    二级:{
+        'title':'.pic&&img&&title',
+        'img':'.pic&&img&&src',
+        'desc':'.jianjie&&ul&&li:eq(4)&&Text',
+        'content':'.jianjie&&ul&&span&&Text',
+        'tabs':'#playtab&&.cili',
+        'lists':'#playtab&&a[href^="magnet:"]'
+    },
+    searchUrl:'/index.php/ajax/suggest?mid=fypage&wd=**',
+    搜索:'json:list;name;pic;;id',
 }
