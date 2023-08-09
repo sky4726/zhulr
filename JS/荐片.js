f7b4b0e76adebb059bf0b535468772a9e6d7cb5c
圆周率 LR <lr.ya@qq.com>
2023-08-09 06:43:38 +0000

    Replace 荐片.js

--- JS/荐片.js
+++ JS/荐片.js
@@ -34,7 +34,7 @@ var rule = {
 	一级:'json:data;title;path;playlist.title;id',
     二级:{
     title:'data.title',
-    desc:'data.score;data.year.title;data.area.title;data.writers[0.1.2.3].name',
+    desc:'data.score;data.year.title;data.area.title;data.writers[0|1|2|3].name',
     img:'data.thumbnail',
     content:'data.description',
     is_json:1,
