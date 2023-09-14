3a88c521af78bb537c0094dacceebcfffee7eb53
圆周率 LR <lr.ya@qq.com>
2023-09-14 10:33:55 +0000

    Replace 腾讯.js

--- JS/腾讯.js
+++ JS/腾讯.js
@@ -6,10 +6,10 @@ var rule = {
     searchUrl:'/x/search/?q=**&stag=fypage',
     multi:1,
     url:'/x/bu/pagesheet/list?_all=1&append=1&channel=fyclass&listpage=1&offset=((fypage-1)*21)&pagesize=21&iarea=-1',
-    filter_url:'sort={{fl.sort or 18}}&year={{fl.year}}&ipay={{fl.ipay}}&ifeature={{fl.ifeature}}&charge={{fl.charge}}&characteristic={{fl.characteristic}}',
+    filter_url:'sort={{fl.sort or 18}}&year={{fl.year}}&iyear={{fl.iyear}}&ipay={{fl.ipay}}&ifeature={{fl.ifeature}}&charge={{fl.charge}}&characteristic={{fl.characteristic}}',
     filter:{'tv': [
      {'key': 'ifeature', 'name': '类型', 'value': [{'n': '全部', 'v': '-1'}, {'n': '爱情', 'v': '1'}, {'n': '古装', 'v': '2'}, {'n': '悬疑', 'v': '3'}, {'n': '都市', 'v': '4'}, {'n': '家庭', 'v': '5'}, {'n': '喜剧', 'v': '6'}, {'n': '传奇', 'v': '7'}, {'n': '武侠', 'v': '8'}, {'n': '军旅', 'v': '9'}, {'n': '权谋', 'v': '10'}, {'n': '革命', 'v': '11'}, {'n': '现实', 'v': '13'}, {'n': '青春', 'v': '14'}, {'n': '猎奇', 'v': '15'}, {'n': '科幻', 'v': '16'}, {'n': '竞技', 'v': '17'}, {'n': '玄幻', 'v': '18'}]},
-     {'key': 'year', 'name': '年代', 'value': [{'n': '全部', 'v': '-1'}, {'n': '2023', 'v': '2023'}, {'n': '2022', 'v': '2022'}, {'n': '2021', 'v': '2021'}, {'n': '2020', 'v': '2020'}, {'n': '2019', 'v': '2019'}, {'n': '2018', 'v': '2018'}, {'n': '2017', 'v': '2017'}, {'n': '00年代', 'v': '2000'}, {'n': '更早', 'v': '99'}]},
+     {'key': 'iyear', 'name': '年代', 'value': [{'n': '全部', 'v': '-1'}, {'n': '2023', 'v': '2023'}, {'n': '2022', 'v': '2022'}, {'n': '2021', 'v': '2021'}, {'n': '2020', 'v': '2020'}, {'n': '2019', 'v': '2019'}, {'n': '2018', 'v': '2018'}, {'n': '2017', 'v': '2017'}, {'n': '00年代', 'v': '2000'}, {'n': '更早', 'v': '99'}]},
      {'key': 'ipay', 'name': '资费', 'value': [{'n': '全部', 'v': '-1'}, {'n': '免费', 'v': '1'}, {'n': '会员', 'v': '2'}, {'n': '短剧', 'v': '11811'}, {'n': '即将上线', 'v': '30'}, {'n': 'X剧场', 'v': '3'}]},
      {'key': 'sort', 'name': '排序', 'value': [{'n': '最热', 'v': '75'}, {'n': '最新', 'v': '79'}, {'n': '好评', 'v': '16'}]}], 
      'movie': [
