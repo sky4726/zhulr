3c028096ab4eed2b7a910dc109f33b2e64fec272
圆周率 LR <lr.ya@qq.com>
2023-10-05 11:41:42 +0000

    Replace MOFLIX.js

--- JS/MOFLIX.js
+++ JS/MOFLIX.js
@@ -24,7 +24,7 @@ var rule = {
     class_name:'',
     class_url:'',
     detailUrl:'/index.php/vod/detail/id/fyid.html',
-    tab_exclude:'高清线路C|在线下载',
+    tab_exclude:'高清线路C|hnm3u8',
     推荐:'.border-box .public-list-box;a&&title;.lazy&&data-original;.public-list-prb&&Text;a&&href',
     double: false, // 推荐内容是否双层定位
     一级:'js:let body=input.split("#")[1];let t=Math.round(new Date/1e3).toString();let key=md5("DS"+t+"DCC147D11943AF75");let url=input.split("#")[0];body=body+"&time="+t+"&key="+key;print(body);fetch_params.body=body;let html=post(url,fetch_params);let data=JSON.parse(html);VODS=data.list.map(function(it){it.vod_pic=it.vod_pic.replace("mac:","https:");return it});',
