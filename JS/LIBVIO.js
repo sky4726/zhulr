30650ec85234adf4651364a86f3e51f65b6abf8f
圆周率 LR <lr.ya@qq.com>
2023-11-26 11:03:46 +0000

    Replace LIBVIO.js

--- JS/LIBVIO.js
+++ JS/LIBVIO.js
@@ -23,7 +23,7 @@ var rule = {
 	},
 	headers:{//网站的请求头,完整支持所有的,常带ua和cookies
 		// 'User-Agent':'MOBILE_UA'
-		'User-Agent':'Mozilla/5.0 (Linux; Android 9; ASUS_Z01QD Build/PQ3B.190801.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/91.0.4472.114 Safari/537.36'
+		'User-Agent':'Mozilla/5.0(Windows NT 10.0;Win64;x64)AppleWebKit/AppleWebKit537.36 (KHTML,like Gecko)Chrome/117.0.0.0 Safari/537.36'
 	},
 	class_parse:'.stui-header__menu li:gt(0):lt(7);a&&Text;a&&href;/(\\d+).html',
 	// class_parse:'.stui-header__menu li;a&&Text;a&&href;/.*_(\\d+).html',
