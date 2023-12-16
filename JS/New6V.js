621d27c2720603485572cb8a56a3ab4d565692da
圆周率 LR <lr.ya@qq.com>
2023-12-16 13:46:16 +0000

    Replace New6V.js

--- JS/New6V.js
+++ JS/New6V.js
@@ -1,57 +1,79 @@
-{
-    "title": "New6V",
-    "author": "压脉带",
-    "Headers": "PC_UA",
-    "Coding_format": "UTF-8",
-    "PicNeedProxy": "0",
-    "homeContent":"1",
-    "firstpage": "1",
-    "class_url": "http://www.xb6v.com/{cateId}/index_{catePg}.html[firstPage=http://www.xb6v.com/{cateId}/index.html]",
-    "class_name": "动画片&综艺&纪录片&国产剧&日韩剧&欧美剧&喜剧片&动作片&爱情片&科幻片&恐怖片&剧情片&战争片",
-    "class_value": "donghuapian&ZongYi&jilupian&dianshiju/guoju&dianshiju/rihanju&dianshiju/oumeiju&xijupian&dongzuopian&aiqingpian&kehuanpian&kongbupian&juqingpian&zhanzhengpian",
-    "filterdata": {},
-    "cat_mode": "1",
-    "catjson_twice": "",
-    "cat_arr_rule": "body&&.post_hover",
-    "cat_is_jsoup": "1",
-    "cat_title": "a&&title",
-    "cat_url": "a&&href",
-    "cat_pic": "img&&src",
-    "cat_subtitle": ".pic-text&&Text",
-    "cat_prefix": "http://www.xb6v.com/",
-    "cat_suffix": "",
-    "SHeaders": "User-Agent$PC_UA",
-    "search_url": "http://www.xb6v.com/e/search/index.php;post",
-    "sea_PtBody": "show=title&tempid=1&tbname=article&mid=1&dopost=search&submit=&keyboard={wd}",
-    "search_mode": "1",
-    "sea_arr_rule": "body&&.post_hover",
-    "sea_is_jsoup": "1",
-    "sea_pic": "img&&src",
-    "sea_title": "font&&Text",
-    "sea_url": "a&&href",
-    "sea_subtitle": "",
-    "search_prefix": "http://www.xb6v.com/",
-    "search_suffix": "",
-    "force_play": "0",
-    "play_prefix": "https://live.52sf.ga/huya/",
-    "play_suffix": "#isVideo=true#",
-    "play_header": "authority$ku.peizq.online#Referer$https://play.peizq.online",
-    "proj_is_jsoup": "0",
-    "proj_cate": "语　　言",
-    "proj_year": "年　　代",
-    "proj_area": "类　　别",
-    "proj_actor": "演　　员　&&</p>",
-    "proj_plot": "简　　介&&</p>",
-    "list_arr_rule": "body&&[href*=magnet]",
-    "epi_arr_rule": "a",
-    "epi_is_jsoup": "1",
-    "epi_title": "a&&Text",
-    "epi_url": "a&&href",
-    "epi_reverse": "0",
-    "epiurl_prefix": "",
-    "epiurl_suffix": "",
-    "Anal_MacPlayer": "0",
-    "ManualSniffer": "0",
-    "VideoFormat": ".mp4#.m3u8#.flv",
-    "VideoFilter": ".html#=http"
+var rule = {
+	title:'New6V',
+	host:'http://www.xb6v.com',
+	// url: '/fyclass/index_(fypage-1).html',
+	url: '/fyclassfyfilter/index_fypage.html[/fyclassfyfilter/index.html]',
+	searchUrl: '/e/search/index.php#show=title&tempid=1&tbname=article&mid=1&dopost=search&submit=&keyboard=**;post',
+	filter_url:'{{fl.class}}',
+	filter:{
+		"dianshiju":[{"key":"class","name":"类型","value":[{"n":"全部","v":""},{"n":"国剧","v":"/guoju"},{"n":"日韩剧","v":"/rihanju"},{"n":"欧美剧","v":"/oumeiju"}]}]
+	},
+	searchable:1,
+	quickSearch:1,
+	filterable:0,
+	headers:{
+		'User-Agent': 'MOBILE_UA'
+	},
+	timeout:10000,
+	class_parse:'#menus&&li:gt(1);a&&Text;a&&href;.*/(.*)/',
+	cate_exclude:'旧版6v|国剧|日韩剧|欧美剧',
+	play_parse:true,
+	lazy:'',
+	limit:12,
+	推荐: '*',
+	一级: '#post_container&&li;h2&&Text;img&&src;.info_category&&Text;a&&href',
+	/*一级: `js:
+		pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+		var d = [];
+		var html = request(input);
+		var list = pdfa(html, 'body&&.post_hover');
+		list.forEach(it => {
+			d.push({
+				title: pdfh(it, 'h2&&Text'),
+				desc: pdfh(it, '.info_category&&Text'),
+				pic_url: pdfh(it, 'img&&src'),
+				url: pd(it, 'a&&href')
+			});
+		})
+		setResult(d);
+	`,*/
+	二级:{
+		title:'.h1&&Text;.info_category&&Text',
+		img:'.mainleft p:eq(0)&&src',
+		content:'.context p:eq(2)&&Text',
+		tabs: `js:
+			TABS = ["磁力播放[观影后,记得清理缓存]"];
+			let tabs = pdfa(html, '#content&&h3:not(:contains(网盘))');
+			tabs.forEach((it) => {
+				TABS.push(pdfh(it, "body&&Text").replace('播放地址','在线播放').replace('（无插件 极速播放）','一').replace('（无需安装插件）','二'))
+			});
+		`,
+		lists: `js:
+			log(TABS);
+			pdfh=jsp.pdfh;pdfa=jsp.pdfa;pd=jsp.pd;
+			LISTS = [];
+			let i = 1;
+			TABS.forEach(function(tab) {
+				if (/磁力播放/.test(tab)) {
+					var d = pdfa(html, '.context&&.firstRow:eq(#id)&&a');
+					d = d.map(function(it) {
+						var title = pdfh(it, 'a&&Text');
+						var burl = pd(it, 'a&&href');
+						return title + '$' + burl
+					});
+					LISTS.push(d)
+				} else if (/在线播放/.test(tab) && i <= TABS.length-1) {
+					var d = pdfa(html, '.context&&.widget:eq(list_idx)&&a'.replace("list_idx", i));
+					d = d.map(function(it) {
+						var title = pdfh(it, 'a&&Text');
+						var burl = pd(it, 'a&&href');
+						return title + '$' + burl
+					});
+					LISTS.push(d)
+					i = i + 1;
+				}
+			});
+		`,
+	},
+	搜索: '*',
 }
